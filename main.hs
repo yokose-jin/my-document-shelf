b4900d98f798fa4609f979404c98ceddd364eb46
横瀬仁 <jin@yokosehitoshinoMacBook-Air.local>
2025-10-08 01:47:03 +0900

    update Main

--- main.hs
+++ main.hs
@@ -4,9 +4,9 @@ import Hakyll
 import Data.Aeson
 import Data.Aeson.TH
 import qualified Data.ByteString.Lazy as BL
--- import Data.List (sortBy, nub)
--- import Data.Ord (comparing)
--- import Data.Maybe (fromMaybe)
+import Data.List (sortBy, nub)
+import Data.Ord (comparing)
+import Data.Maybe (fromMaybe)
 import Modules.HTMLTreeTypes
 
 main :: IO ()
